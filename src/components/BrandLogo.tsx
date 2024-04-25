fd8fb4597257fde7bf4aabf598d386d39ccc6823
MaxCojocari <cojocarigoncearmax@gmail.com>
2024-04-25 23:37:51 +0300

    feat: add logo

---
+++ src/components/BrandLogo.tsx
@@ -0,0 +1,32 @@
+import { Box, Typography } from "@mui/material";
+
+export default function BrandLogo() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        gap: "10px",
+      }}
+    >
+      <Box
+        component="img"
+        sx={{ width: 30, height: "auto" }}
+        alt="CourseMe Logo"
+        src="logo.svg"
+      />
+      <Typography
+        variant="h6"
+        component="p"
+        sx={{
+          fontWeight: 700,
+          fontStyle: "normal",
+          fontSize: "18px",
+          color: "#545be8",
+        }}
+      >
+        CourseMe
+      </Typography>
+    </Box>
+  );
+}
