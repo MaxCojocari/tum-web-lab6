9c1aa141e501b8830a7ba78a444d5d613e8cbc7e
MaxCojocari <cojocarigoncearmax@gmail.com>
2024-04-25 23:48:05 +0300

    feat: add search bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,36 @@
 import styled from "@emotion/styled";
-import { AppBar, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import BrandLogo from "./BrandLogo";
+import { Mail, Notifications } from "@mui/icons-material";
+import React, { useState } from "react";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
-  backgroundColor: "red",
-  boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.15)",
+  backgroundColor: "white",
+  padding: "6px",
+});
+
+const Search = styled("div")({
+  backgroundColor: "white",
+  padding: "0 10px",
+  width: "50%",
+  borderRadius: "10px",
+  border: "1px solid #ccc",
+});
+
+const Icons = styled(Box)({
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
 });
 
 export default function Navbar() {
@@ -14,6 +38,10 @@ export default function Navbar() {
     <AppBar position="sticky">
       <StyledToolbar>
         <BrandLogo />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons></Icons>
       </StyledToolbar>
     </AppBar>
   );
