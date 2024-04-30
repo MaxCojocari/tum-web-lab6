6b9d333364cb5db6095c4a0069f1adafbf33047e
MaxCojocari <cojocarigoncearmax@gmail.com>
2024-04-30 23:53:48 +0300

    feat: add navigation

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,17 @@
 import { styled } from "@mui/material/styles";
-import { Paper } from "@mui/material";
+import { useState } from "react";
+import {
+  Collapse,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Paper,
+} from "@mui/material";
+import { ExpandLess, ExpandMore, StarBorder } from "@mui/icons-material";
+import HomeIcon from "@mui/icons-material/Home";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
 
 const StyledSidebar = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -14,9 +26,44 @@ const StyledSidebar = styled(Paper)(({ theme }) => ({
 }));
 
 export function Sidebar() {
+  const [open, setOpen] = useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
   return (
     <>
-      <StyledSidebar></StyledSidebar>
+      <StyledSidebar>
+        <ListItemButton>
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText primary="Home" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <FavoriteBorderIcon />
+          </ListItemIcon>
+          <ListItemText primary="Favorite" />
+        </ListItemButton>
+        <ListItemButton onClick={handleClick}>
+          <ListItemIcon>
+            <FilterAltIcon />
+          </ListItemIcon>
+          <ListItemText primary="Filter Courses" />
+          {open ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItemButton sx={{ pl: 4 }}>
+              <ListItemIcon>
+                <StarBorder />
+              </ListItemIcon>
+              <ListItemText primary="Starred" />
+            </ListItemButton>
+          </List>
+        </Collapse>
+      </StyledSidebar>
     </>
   );
 }
