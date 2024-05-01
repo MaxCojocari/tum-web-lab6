6da5a7693b6143b64280b0b6dec28ffe0fde35e7
MaxCojocari <cojocarigoncearmax@gmail.com>
2024-05-01 18:23:31 +0300

    refactor: encapsulate analogical layout

---
+++ src/components/PageBlueprint.tsx
@@ -0,0 +1,38 @@
+import { Box, Grid, Typography, useMediaQuery } from "@mui/material";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import { Sidebar } from "./Sidebar";
+import { theme } from "../theme";
+import CoursesList from "./CoursesList";
+
+export default function PageBlueprint({ mode, setMode, pageName }: any) {
+  const isNarrowScreen = useMediaQuery("(max-width:1200px)");
+  const columns = isNarrowScreen ? 4 : 3;
+
+  return (
+    <Box>
+      <Navbar setMode={setMode} mode={mode} />
+      <Grid container spacing={3} sx={{ padding: "90px 90px 0px 90px" }}>
+        <Grid item xs={columns}>
+          <Sidebar />
+        </Grid>
+        <Grid item xs={12 - columns}>
+          <Typography
+            variant="h6"
+            sx={{
+              fontFamily: "Poppins, sans-serif",
+              fontWeight: "bold",
+              fontSize: "40px",
+              color: theme.palette.mode === "light" ? "#141e32" : "#fff",
+              margin: "-16px 0 20px 0",
+            }}
+          >
+            {pageName}
+          </Typography>
+          <CoursesList />
+        </Grid>
+      </Grid>
+      <Footer />
+    </Box>
+  );
+}
