e3980fd9495b30e9c8c6634caca2748e6ff870cd
MaxCojocari <cojocarigoncearmax@gmail.com>
2024-05-01 11:00:54 +0300

    feat: design general grid layout

--- src/components/CoursesList.tsx
+++ src/components/CoursesList.tsx
@@ -1,12 +1,19 @@
-import Course from "./Course";
+import { Grid } from "@mui/material";
+import CourseCard from "./CourseCard";
 
 export default function CoursesList() {
   return (
-    <>
-      <Course />
-      <Course />
-      <Course />
-      <Course />
-    </>
+    <Grid
+      container
+      spacing={{ xs: 2, md: 3 }}
+      columns={{ xs: 4, sm: 8, md: 12 }}
+      sx={{ paddingRight: "16px", paddingLeft: "10px" }}
+    >
+      {Array.from(Array(12)).map((_, index) => (
+        <Grid item xs={2} sm={4} md={4} key={index}>
+          <CourseCard />
+        </Grid>
+      ))}
+    </Grid>
   );
 }
