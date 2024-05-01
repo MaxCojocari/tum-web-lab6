d760ca9d492ec9128ae6a14633092c2da453b7e5
MaxCojocari <cojocarigoncearmax@gmail.com>
2024-05-01 22:20:58 +0300

    refactor: add re-rendering when an item is deleted

--- src/components/RemoveButton.tsx
+++ src/components/RemoveButton.tsx
@@ -1,15 +1,27 @@
 import { Button } from "@mui/material";
-import { useState } from "react";
 
-export default function RemoveButton() {
-  const [clicked, setClicked] = useState(false);
+interface RemoveButtonProps {
+  parentCourseId: number;
+  onItemRemoved: () => void;
+}
+
+export default function RemoveButton({
+  parentCourseId,
+  onItemRemoved,
+}: RemoveButtonProps) {
+  function remove() {
+    const cartContentIds = localStorage.getItem("cart");
+    let ids = cartContentIds ? JSON.parse(cartContentIds) : [];
+
+    ids = ids.filter((item: number) => item !== parentCourseId);
+    ids.sort((a: number, b: number) => a - b);
+    localStorage.setItem("cart", JSON.stringify(ids));
+
+    onItemRemoved();
+  }
 
   return (
-    <Button
-      variant="contained"
-      color="error"
-      onClick={(e) => setClicked(!clicked)}
-    >
+    <Button variant="contained" color="error" onClick={remove}>
       REMOVE
     </Button>
   );
